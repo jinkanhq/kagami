f36962b5c0717375e26d153b13cfc0c627930dcf
yinian1992 <yinian@jinkan.org>
2020-10-24 22:03:06 +0800

    feat: add web fonts
    1. add 4 web fonts in WOFF2 format
    2. fix the width of image in post content

--- assets/css/variables.css.php
+++ assets/css/variables.css.php
@@ -1,5 +1,10 @@
-<?php header( 'Content-Type: text/css')  ?>
-<?php header( 'Etag: ' . md5_file( __FILE__ ) )?>
+<?php 
+header( 'Content-Type: text/css');
+header( 'Etag: ' . md5_file( __FILE__ ) );
+$jsdelivr_used = get_theme_mod('jsdelivr_used', false);
+$jsdelivr_font_base_path = 'https://cdn.jsdelivr.net/gh/jinkanhq/kagami@v' . KAGAMI_VERSION . '/assets/fonts/';
+$font_base_path = $jsdelivr_used ? $jsdelivr_font_base_path : get_template_directory_uri() . '/assets/fonts/';
+?>
 
 :root {
     --primary-color: <?php echo get_theme_mod('primary_color', '#6b7dd6'); ?>;
@@ -7,10 +12,34 @@
     --text-color: <?php echo get_theme_mod('text_color', '#333'); ?>;
     --link-color: <?php echo get_theme_mod('link_color', '#4160ff'); ?>;
     --white-color: <?php echo get_theme_mod('white_color', '#f7f7f7'); ?>;
-    --sans-serif-font-family: <?php echo get_theme_mod('sans_serif_font_family', '-apple-system,BlinkMacSystemFont,opensans,Optima,"Source Han Sans CN","Noto Sans CJK SC","Microsoft Yahei",sans-serif'); ?>;
-    --serif-font-family: <?php echo get_theme_mod('serif_font_family', '"Source Han Serif CN", Cambria, Georgia, Cochin, "Times New Roman", Times, serif;'); ?>;
+    --sans-serif-font-family: <?php echo get_theme_mod('sans_serif_font_family', '"Source Han Sans CN", sans-serif'); ?>;
+    --serif-font-family: <?php echo get_theme_mod('serif_font_family', '"Source Han Serif CN", serif;'); ?>;
     --logo-image: url(<?php echo kagami_get_theme_mod_image_url('logo_image', '../images/lab_logo_hori_white.png') ?>);
     --footer-gradient-start-color: <?php echo get_theme_mod('footer_gradient_start_color', '#6b7dd6'); ?>;
     --footer-gradient-end-color: <?php echo get_theme_mod('footer_gradient_end_color', '#001689'); ?>;
     --fill-background-image: url(<?php echo kagami_get_theme_mod_image_url('fill_background_image', '../images/light_toast.png'); ?>);
+}
+
+@font-face {
+    font-family: 'Source Han Sans CN';
+    font-weight: 300;
+    src: url('<?php echo $font_base_path . 'SourceHanSansCN-Light.woff2'?>');
+}
+
+@font-face {
+    font-family: 'Source Han Sans CN';
+    font-weight: 400;
+    src: url('<?php echo $font_base_path . 'SourceHanSansCN-Regular.woff2'?>');
+}
+
+@font-face {
+    font-family: 'Source Han Serif CN';
+    font-weight: 300;
+    src: url('<?php echo $font_base_path . 'SourceHanSerifCN-Light.woff2'?>');
+}
+
+@font-face {
+    font-family: 'Source Han Serif CN';
+    font-weight: 500;
+    src: url('<?php echo $font_base_path . 'SourceHanSerifCN-Medium.woff2'?>');
 }
--- inc/constants.php
+++ inc/constants.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'KAGAMI_VERSION', '1.0' );
+define( 'KAGAMI_VERSION', '1.1' );
 define( 'KAGAMI_REPO', 'https://github.com/jinkanhq/kagami' );
 define( 'KAGAMI_FALLBACK_COVER_URL', get_theme_file_uri( '/assets/images/fallback_cover.jpg' ) );
 define( 'KAGAMI_FALLBACK_PAGE_NOT_FOUND_COVER_URL', get_theme_file_uri( '/assets/images/fallback_archive_cover.jpg' ) );
